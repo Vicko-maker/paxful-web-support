f0a45d8033ab691671367972d27c53c682bca872
Vicko-maker <korirkelvinkipkirui@gmail.com>
2024-11-26 18:50:30 +0300

    Commit

--- paxapp/admin.py
+++ paxapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Person
 
-
-admin.site.register(Person)
-
-list_display = ('id', 'email', 'username')
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    # Fields to display in the admin list view
+    list_display = ('id', 'email', 'username')  # Replace with actual fields from your model
+    list_filter = ('email',)  # Optional: Adds filters for specific fields
+    search_fields = ('email', 'username')  # Optional: Adds a search box
